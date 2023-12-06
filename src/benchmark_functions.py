61a12238aba92db1bd8c9b54f6985c4ffae21690
Murf-y <charbelfayad64@gmail.com>
2023-12-06 19:27:58 +0200

    remove logs

--- src/benchmark_functions.py
+++ src/benchmark_functions.py
@@ -4,27 +4,11 @@ import random
 import numpy as np
 import math
 import os
-from tsp.tsp import tsp
-from cec2017.cec2017 import get_cec2017
-
-# Ackely - acceptance : 0.01
-# multimodal
-f1: GABenchmarkFunction = GABenchmarkFunction(
-    name="f1",
-    create_random_individual=lambda nb_genes, domain, decimal_precision: [
-        round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
-    fitness_function=lambda individual: 20 - 20 *
-    math.exp(-0.2*math.sqrt((1/30) *
-                            sum([individual[i]**2 for i in range(len(individual))]))) + math.e - math.exp((1/30) * sum(
-                                [math.cos(2*math.pi*individual[i]) for i in range(len(individual))])),
-    decimal_precision=0,
-    is_maximization=False, lower_bound=-32, upper_bound=32, number_of_genes=30, optimal_value=0,
-    function_characteristics=[FunctionCharacteristic.MULTIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_INVARIENT])
 
 # De Jong - acceptance : 0.01
 # unimodal
-f2: GABenchmarkFunction = GABenchmarkFunction(
-    name="f2",
+f1: GABenchmarkFunction = GABenchmarkFunction(
+    name="f1",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: sum(
@@ -33,22 +17,10 @@ f2: GABenchmarkFunction = GABenchmarkFunction(
     is_maximization=False, lower_bound=-100, upper_bound=100, number_of_genes=30, optimal_value=0,
     function_characteristics=[FunctionCharacteristic.UNIMODAL, FunctionCharacteristic.SEPARETABLE, FunctionCharacteristic.SYMMETRIC, FunctionCharacteristic.PERMUTATION_INVARIENT])
 
-# Unintresting function - acceptance : 0.01
-# unimodal
-f3: GABenchmarkFunction = GABenchmarkFunction(
-    name="f3",
-    create_random_individual=lambda nb_genes, domain, decimal_precision: [
-        round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
-    fitness_function=lambda individual: sum(
-        [abs(individual[i]) for i in range(len(individual))]) + np.prod([abs(individual[i]) for i in range(len(individual))]),
-    decimal_precision=0,
-    is_maximization=False, lower_bound=-10, upper_bound=10, number_of_genes=30, optimal_value=0,
-    function_characteristics=[FunctionCharacteristic.UNIMODAL, FunctionCharacteristic.SEPARETABLE, FunctionCharacteristic.SYMMETRIC, FunctionCharacteristic.PERMUTATION_INVARIENT])
-
 # acceptance : 0.01
 # unimodal
-f4: GABenchmarkFunction = GABenchmarkFunction(
-    name="f4",
+f2: GABenchmarkFunction = GABenchmarkFunction(
+    name="f2",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: sum(
@@ -59,8 +31,8 @@ f4: GABenchmarkFunction = GABenchmarkFunction(
 
 # rastring acceptance : 50
 # multimodal
-f5: GABenchmarkFunction = GABenchmarkFunction(
-    name="f5",
+f3: GABenchmarkFunction = GABenchmarkFunction(
+    name="f3",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: sum(
@@ -71,8 +43,8 @@ f5: GABenchmarkFunction = GABenchmarkFunction(
 
 # griewank acceptance : 0.01
 # multimodal
-f6: GABenchmarkFunction = GABenchmarkFunction(
-    name="f6",
+f4: GABenchmarkFunction = GABenchmarkFunction(
+    name="f4",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: sum([(individual[i]**2)/4000 for i in range(len(individual))]) - np.prod(
@@ -83,8 +55,8 @@ f6: GABenchmarkFunction = GABenchmarkFunction(
 
 # rosenbrock acceptance : 100
 # unimodal
-f7: GABenchmarkFunction = GABenchmarkFunction(
-    name="f7",
+f5: GABenchmarkFunction = GABenchmarkFunction(
+    name="f5",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: sum(
@@ -93,18 +65,8 @@ f7: GABenchmarkFunction = GABenchmarkFunction(
     is_maximization=False, lower_bound=-32, upper_bound=32, number_of_genes=30, optimal_value=0,
     function_characteristics=[FunctionCharacteristic.MULTIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
 
-eggholder: GABenchmarkFunction = GABenchmarkFunction(
-    name="eggholder",
-    create_random_individual=lambda nb_genes, domain, decimal_precision: [
-        round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
-    fitness_function=lambda individual: -(individual[1] + 47) * math.sin(math.sqrt(abs(individual[0]/2 + (
-        individual[1] + 47)))) - individual[0] * math.sin(math.sqrt(abs(individual[0] - (individual[1] + 47)))),
-    decimal_precision=0,
-    is_maximization=False, lower_bound=-512, upper_bound=512, number_of_genes=2, optimal_value=-959.6407,
-    function_characteristics=[FunctionCharacteristic.MULTIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
-
-dixonprice: GABenchmarkFunction = GABenchmarkFunction(
-    name="dixonprice",
+f6: GABenchmarkFunction = GABenchmarkFunction(
+    name="f6",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: (individual[0] - 1)**2 + sum([(i+1) * (
@@ -114,19 +76,8 @@ dixonprice: GABenchmarkFunction = GABenchmarkFunction(
 
     function_characteristics=[FunctionCharacteristic.UNIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
 
-bent_cigar: GABenchmarkFunction = GABenchmarkFunction(
-    name="bent_cigar",
-    create_random_individual=lambda nb_genes, domain, decimal_precision: [
-        round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
-    fitness_function=lambda individual: individual[0]**2 + 10**6 * sum(
-        [individual[i]**2 for i in range(1, len(individual))]),
-    decimal_precision=0,
-    is_maximization=False, lower_bound=-100, upper_bound=100, number_of_genes=30, optimal_value=0,
-    function_characteristics=[FunctionCharacteristic.UNIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
-
-
-discus: GABenchmarkFunction = GABenchmarkFunction(
-    name="discus",
+f7: GABenchmarkFunction = GABenchmarkFunction(
+    name="f7",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: (10**6)*individual[0]**2 + sum(
@@ -135,60 +86,14 @@ discus: GABenchmarkFunction = GABenchmarkFunction(
     is_maximization=False, lower_bound=-100, upper_bound=100, number_of_genes=30, optimal_value=0,
     function_characteristics=[FunctionCharacteristic.UNIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
 
-schwefel: GABenchmarkFunction = GABenchmarkFunction(
-    name="schwefel",
+f8: GABenchmarkFunction = GABenchmarkFunction(
+    name="f8",
     create_random_individual=lambda nb_genes, domain, decimal_precision: [
         round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
     fitness_function=lambda individual: 418.9829 * len(individual) - sum(
         [individual[i] * math.sin(math.sqrt(abs(individual[i]))) for i in range(len(individual))]),
     decimal_precision=4,
     is_maximization=False, lower_bound=-500, upper_bound=500, number_of_genes=30, optimal_value=0,
-    function_characteristics=[FunctionCharacteristic.MULTIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
-
-
-target_wave_params = [1.0, 5.0, 1.5, 4.8, 2.0, 4.9]
-theta = 2 * math.pi / 100
-
-
-def wave(t, a1, w1, a2, w2, a3, w3):
-    return a1 * math.sin(w1 * t) + a2 * math.sin(w2 * t) + a3 * math.sin(w3 * t)
-
-
-#  Precompute the target wave to save time
-target_wave = [wave(t, *target_wave_params) for t in range(100)]
-
-frequency_modulated_sound_waves: GABenchmarkFunction = GABenchmarkFunction(
-    name="fm_sound_waves",
-    create_random_individual=lambda nb_genes, domain, decimal_precision: [
-        round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
-    fitness_function=lambda individual: sum(
-        [(wave(t, *individual) - target_wave[t])**2 for t in range(100)]),
-    decimal_precision=1,
-    is_maximization=False, lower_bound=-6.4, upper_bound=6.4, number_of_genes=6, optimal_value=0,
-    function_characteristics=[FunctionCharacteristic.MULTIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
-
-
-a = [1.0, 1.2, 3.0, 3.2]
-A = [[3, 10, 30],
-     [0.1, 10, 35],
-     [3, 10, 30],
-     [0.1, 10, 35]]
-
-P = [[0.3689, 0.1170, 0.2673],
-     [0.4699, 0.4387, 0.7470],
-     [0.1091, 0.8732, 0.5547],
-     [0.0381, 0.5743, 0.8828]]
-
-
-HARTMANN: GABenchmarkFunction = GABenchmarkFunction(
-    name="hartmann3",
-    create_random_individual=lambda nb_genes, domain, decimal_precision: [
-        round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
-    fitness_function=lambda individual: -
-    sum([a[i] * math.exp(-sum([A[i][j] * (individual[j] - P[i][j])
-        ** 2 for j in range(3)])) for i in range(4)]),
-    decimal_precision=6,
-    is_maximization=False, lower_bound=0, upper_bound=1, number_of_genes=3, optimal_value=-3.32237,
     function_characteristics=[FunctionCharacteristic.MULTIMODAL, FunctionCharacteristic.NON_SEPARETABLE, FunctionCharacteristic.NON_SYMMETRIC, FunctionCharacteristic.PERMUTATION_VARIANT])
 
 
@@ -202,6 +107,7 @@ def get_all_paper_functions():
     functions.append(f5)
     functions.append(f6)
     functions.append(f7)
+    functions.append(f8)
 
     return functions
 
@@ -216,29 +122,7 @@ def get_all_functions():
     functions.append(f5)
     functions.append(f6)
     functions.append(f7)
-    functions.append(tsp)
-    functions.append(frequency_modulated_sound_waves)
-    functions.append(eggholder)
-    functions.append(dixonprice)
-    functions.append(HARTMANN)
-    functions.append(bent_cigar)
-    functions.append(discus)
-    functions.append(schwefel)
-
-    return functions
-
-
-def final_chosen_functions():
-    functions: List[GABenchmarkFunction] = []
-
-    functions.append(f2)
-    functions.append(f4)
-    functions.append(f5)
-    functions.append(f6)
-    functions.append(f7)
-    functions.append(dixonprice)
-    functions.append(discus)
-    functions.append(schwefel)
+    functions.append(f8)
 
     return functions
 
@@ -267,22 +151,5 @@ def get_functions_by_name(names: List[str]):
 
     if len(functions) == 0:
         raise Exception("No function with the given names")
-
-    return functions
-
-
-def get_cec_2017_functions():
-    functions: List[GABenchmarkFunction] = []
-
-    for func_name, func in get_cec2017().items():
-        functions.append(GABenchmarkFunction(
-            name=func_name,
-            create_random_individual=lambda nb_genes, domain, decimal_precision: [
-                round(random.uniform(domain[0], domain[1]), decimal_precision) for _ in range(nb_genes)],
-            fitness_function=lambda individual: func(
-                np.array([individual]))[0],
-            decimal_precision=0,
-            is_maximization=False, lower_bound=-100, upper_bound=100, number_of_genes=30, optimal_value=0,
-            function_characteristics=[]))
 
     return functions
--- src/main.py
+++ src/main.py
@@ -2,20 +2,14 @@ import random
 from selection import SelectionType
 from mutation import MutationType
 from crossover import CrossOverType
-from genetic_algorithm_mod import GeneticAlgorithmModified
 from genetic_algorithm import GeneticAlgorithm, GAHistory
-from enhanced_networked_genetic_algorithm import SAEnhancedNetworkGeneticAlgorithm, EnhancedNetworkGeneticAlgorithm, OnlySelectionENGA
-from networked_genetic_algorithm import NetworkGeneticAlgorithm, OnlySelectionNGA
+from enhanced_networked_genetic_algorithm import EnhancedNetworkGeneticAlgorithm
+from networked_genetic_algorithm import NetworkGeneticAlgorithm
 from benchmark import Benchmark, GABenchmarkFunction, FunctionCharacteristic
-from benchmark_functions import get_functions_by_name, get_cec_2017_functions, get_functions_with_characteristics, get_all_paper_functions, final_chosen_functions
-from genetic_algorithm_copy import GeneticAlgorithmCopy
+from benchmark_functions import get_functions_by_name, get_functions_with_characteristics
 import sys
 import math
-from ga_random import GA_RANDOM
 from ynga_community import YNGA
-from ynga2 import YNGA2
-from ynga3 import YNGA3
-from genetic_algorithm_strict_selection import GeneticAlgorithmStrictSelection
 import matplotlib.pyplot as plt
 
 sys.path.append('.')
@@ -23,47 +17,20 @@ sys.path.append('.')
 
 def main():
     benchmark_functions = get_functions_by_name([
-        'f2', 'f4', 'f5', 'f6', 'f7', 'dixonprice', 'discus', 'schwefel'
+        'f2',
     ])
 
     enhanced_nga_benchmark = Benchmark(genetic_algorithm=EnhancedNetworkGeneticAlgorithm, number_of_runs=10, benchmark_functions=benchmark_functions,
                                        number_of_generations=2000, population_size=125, number_elites=0, probability_crossover=0.95, probability_mutation=0.2, verbose=True, save_metrics=False, comment="official run mutation 0.2")
 
-    only_selection_enga_benchmark = Benchmark(genetic_algorithm=OnlySelectionENGA, number_of_runs=1, benchmark_functions=benchmark_functions, number_of_generations=2000,
-                                              population_size=125, number_elites=0, probability_crossover=0.95, probability_mutation=0.2, verbose=True, save_metrics=True, comment="s")
-
-    saenhanced_nga_benchmark = Benchmark(genetic_algorithm=SAEnhancedNetworkGeneticAlgorithm, number_of_runs=10, benchmark_functions=benchmark_functions,
-                                         number_of_generations=2000, population_size=125, number_elites=10, probability_crossover=1, probability_mutation=0.5, verbose=True, save_metrics=False, comment="strict mating")
-
-    nga_benchmark = Benchmark(genetic_algorithm=NetworkGeneticAlgorithm, number_of_runs=10, benchmark_functions=benchmark_functions,
+    nga_benchmark = Benchmark(genetic_algorithm=NetworkGeneticAlgorithm, number_of_runs=1, benchmark_functions=benchmark_functions,
                               number_of_generations=2000, population_size=125, number_elites=0, probability_crossover=0.95, probability_mutation=0.1, verbose=True, save_metrics=False, comment="official run mutation 0.2")
-
-    ga_random_benchmark = Benchmark(genetic_algorithm=GA_RANDOM, number_of_runs=1, benchmark_functions=benchmark_functions,
-                                    number_of_generations=2000, population_size=125, number_elites=0, probability_crossover=0.95, probability_mutation=0.1, verbose=True, save_metrics=False, comment="GA RANDOM")
-
-    only_selection_nga_benchmark = Benchmark(genetic_algorithm=OnlySelectionNGA, number_of_runs=1, benchmark_functions=benchmark_functions,
-                                             number_of_generations=2000, population_size=125, number_elites=0, probability_crossover=0.95, probability_mutation=0.1, verbose=True, save_metrics=False,)
-
-    ga_mod_benchmark = Benchmark(genetic_algorithm=GeneticAlgorithmModified, number_of_runs=1, benchmark_functions=benchmark_functions,
-                                 number_of_generations=2000, population_size=125, number_elites=0, probability_crossover=1, probability_mutation=0.4, verbose=True, save_metrics=False,)
 
     ga_benchmark = Benchmark(genetic_algorithm=GeneticAlgorithm, number_of_runs=50, benchmark_functions=benchmark_functions,
                              number_of_generations=2000, population_size=125, number_elites=10, probability_crossover=0.95, probability_mutation=0.2, verbose=True, save_metrics=False, comment="official run mutation 0.2")
 
-    ga_copy_benchmark = Benchmark(genetic_algorithm=GeneticAlgorithmCopy, number_of_runs=5, benchmark_functions=benchmark_functions,
-                                  number_of_generations=2000, population_size=125, number_elites=20, probability_crossover=0.95, probability_mutation=0.2, verbose=True, save_metrics=False,)
-
-    ga_strict_benchmark = Benchmark(genetic_algorithm=GeneticAlgorithmStrictSelection, number_of_runs=1, benchmark_functions=benchmark_functions,
-                                    number_of_generations=2000, population_size=125, number_elites=20, probability_crossover=1, probability_mutation=0.1, verbose=False, save_metrics=False,)
-
     ynga_benchmark = Benchmark(genetic_algorithm=YNGA, number_of_runs=1, benchmark_functions=benchmark_functions,
                                number_of_generations=2000, population_size=124, number_elites=20, probability_crossover=1, probability_mutation=0.25, verbose=True, save_metrics=False,)
-
-    ynga2_benchmark = Benchmark(genetic_algorithm=YNGA2, number_of_runs=3, benchmark_functions=benchmark_functions,
-                                number_of_generations=2000, population_size=125, number_elites=20, probability_crossover=1, probability_mutation=0.1, verbose=True, save_metrics=False,)
-    ynga3_benchmark = Benchmark(genetic_algorithm=YNGA3, number_of_runs=3, benchmark_functions=benchmark_functions,
-                                number_of_generations=2000, population_size=124, number_elites=20, probability_crossover=1, probability_mutation=0.2, verbose=True, save_metrics=False,)
-
     # runs = {}
     # start_mutation = 0.1
     # end_mutation = 0.8
@@ -92,18 +59,10 @@ def main():
     #     plt.legend()
     #     plt.show()
 
-    enhanced_nga_benchmark.run()
-    # only_selection_enga_benchmark.run()
-    # saenhanced_nga_benchmark.run()
-    # nga_benchmark.run()
-    # ga_random_benchmark.run()
-    # only_selection_nga_benchmark.run()
-    # ga_mod_benchmark.run()
+    # enhanced_nga_benchmark.run()
+    nga_benchmark.run()
     # ga_benchmark.run()
-    # ga_copy_benchmark.run()
-    # ga_strict_benchmark.run()
-    # ynga2_benchmark.run()
-    # ynga3_benchmark.run()
+    # ynga_benchmark.run()
 
 
 if __name__ == '__main__':
